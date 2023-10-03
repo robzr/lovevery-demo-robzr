d6f87c8eb7ee00b627346059b39c363f44ae513c
Rob Zwissler <rob@zwissler.org>
2023-10-03 11:24:06 -0700

    Update Rails stubs for our app

--- app/config/environments/development.rb
+++ app/config/environments/development.rb
@@ -67,4 +67,12 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.web_console.allowed_ips = [
+    '10.0.0.0/8',
+    '172.16.0.0/12',
+    '192.168.0.0/16',
+  ]
+  config.web_console.default_mount_path = '/debug'
+  config.hosts.clear
 end
